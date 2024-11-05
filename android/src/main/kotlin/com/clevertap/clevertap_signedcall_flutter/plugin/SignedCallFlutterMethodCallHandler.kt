ec0d2fb65dd511f610222c9a55550a929c34bd8b
anush <anush@clevertap.com>
2024-11-05 11:35:24 +0530

    task(VC-878) - Replaces the deprecated way to setting missedCall handler with the latest setMissedCallNotificationOpenedHandler API
    - Uses this new API from within the plugin
    - Updates to use the newer setMissedCallActionsList

--- android/src/main/kotlin/com/clevertap/clevertap_signedcall_flutter/plugin/SignedCallFlutterMethodCallHandler.kt
+++ android/src/main/kotlin/com/clevertap/clevertap_signedcall_flutter/plugin/SignedCallFlutterMethodCallHandler.kt
@@ -180,9 +180,6 @@ class SignedCallFlutterMethodCallHandler(
                 parseMissedCallActionsFromInitOptions(it as Map<*, *>)
             }
 
-            val missedCallActionClickHandlerPath =
-                MissedCallActionClickHandler::class.java.canonicalName
-
             val notificationPermissionRequired =
                 initProperties.getOrElse(KEY_NOTIFICATION_PERMISSION_REQUIRED) { true } as Boolean
 
@@ -190,13 +187,14 @@ class SignedCallFlutterMethodCallHandler(
                 parseSwipeOffBehaviourFromInitOptions(it as String)
             } ?: END_CALL
 
+            SignedCallAPI.getInstance().setMissedCallNotificationOpenedHandler(MissedCallActionClickHandler())
             val initConfiguration =
                 SignedCallInitConfiguration.Builder(initOptions, allowPersistSocketConnection)
                     .promptPushPrimer(pushPrimerConfig)
                     .promptReceiverReadPhoneStatePermission(promptReceiverReadPhoneStatePermission)
                     .setNotificationPermissionRequired(notificationPermissionRequired)
                     .overrideDefaultBranding(callScreenBranding)
-                    .setMissedCallActions(missedCallActionsList, missedCallActionClickHandlerPath)
+                    .setMissedCallActions(missedCallActionsList)
                     .setSwipeOffBehaviourInForegroundService(swipeOffBehaviour)
                     .build()
 
